99772e4f646f49aa0503599c6fd064afee7a766b
Dolores <dee12098@yandex.ru>
2023-01-29 16:53:35 +0300

    finish project

--- test/Program.cs
+++ test/Program.cs
@@ -1,27 +1,41 @@
-﻿string[] array = new string[] {"gjr", "egslk", "24:)", "$5"};
+﻿string[] ReadArray()
+{
+    Console.WriteLine("Введите размер массива: ");
+    int sizeOfArray = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[sizeOfArray];
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} эллемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
 
+}
 
-int FindSizeNewArray (string[] arr)
+int FindSizeNewArray(string[] arr)
 {
     int count = 0;
-    for(int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        if(arr[i].Length <= 3)
+        if (arr[i].Length <= 3)
         {
             count++;
         }
     }
+    if (count == 0) Console.WriteLine("В массиве нет элементов, удовлетворяющих условие.");
     return count;
 }
 
-string[] FillNewArray (string[] arr, int size)
+string[] FillNewArray(string[] arr, int size)
 {
     string[] newArray = new string[size];
-    for(int i = 0; i < arr.Length; i++)
+    int i = 0;
+    foreach (string item in arr)
     {
-        if(arr[i].Length <= 3)
+        if (item.Length <= 3)
         {
-            newArray[i] = arr[i];
+            newArray[i] = item;
+            i++;
         }
     }
     return newArray;
@@ -29,14 +43,21 @@ string[] FillNewArray (string[] arr, int size)
 
 void PrintArray(string[] arr)
 {
-    for(int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        Console.Write($"{arr[i]} ");
+        if (arr[i] != string.Empty)
+        {
+            Console.Write($"{arr[i]} ");
+        }
     }
+
 }
 
+string[] firstArray = ReadArray();
 
+int sizeOfSecondArray = FindSizeNewArray(firstArray);
 
-int size = FindSizeNewArray(array);
-Console.WriteLine(size);
-PrintArray(array);
+string[] secondArray = FillNewArray(firstArray, sizeOfSecondArray);
+
+PrintArray(secondArray);
+
