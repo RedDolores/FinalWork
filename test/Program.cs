f40725e9bfc2a26e7c1e927025b772b9f69bff24
Dolores <dee12098@yandex.ru>
2023-01-29 14:46:30 +0300

    add FindSizeNewArray

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,18 @@
 ﻿string[] array = new string[] {"gjr", "egslk", "24:)", "$5"};
-Console.Write(array[0].Length);
+
+
+int FindSizeNewArray (string[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int size = FindSizeNewArray(array);
+Console.WriteLine(size);
